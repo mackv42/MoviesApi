e74e0b2c2fc5fafc2a5d4f0839a7d8a31e04dc76
Vincent Mackay <mackv42@gmail.com>
2019-08-06 14:21:45 -0500

    Post to server Complete

--- MovieApi/Controllers/MovieController.cs
+++ MovieApi/Controllers/MovieController.cs
@@ -3,27 +3,54 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
+using System.Web.Helpers;
+using Newtonsoft.Json.Linq;
 
 namespace MovieApi.Controllers
 {
     public class MovieController : ApiController
     {
         // GET: Movie
+        private ApplicationDbContext context;
+        public MovieController()
+        {
+            context = new ApplicationDbContext();
+        }
+        [HttpGet]
         public List<Movie> Get()
         {
-            return new List<Movie>();
+            return context.Movies.ToList();
         }
         // GET: Movie/Details/5
-        public string Get(int id)
+        public Movie Get(int id)
         {
+            var movie = context.Movies.Where(x => x.Id == id).FirstOrDefault();
             //Gets 
-            return "";
+            return movie;
         }
 
-        public void Post([FromBody]Movie value)
+        [HttpPost]
+        public void Post([FromBody]JObject value)
         {
-            // Create movie in db logic
+            JToken token = value;
+            Movie add = new Movie();
+            add.Director = (string)token.SelectToken("Director");
+            add.Title = (string)token.SelectToken("Title");
+            add.Genre = (string)token.SelectToken("Genre");
+            context.Movies.Add(add);
+            context.SaveChanges();
+            //int totalPages = (int)token.SelectToken("total_pages");
+            /*
+            Movie add = new Movie();
+            add.Director = value.Value(new Movie());
+            add.Title = value[0]["Title"];
+            add.Genre = value[0]["Genre"];
+
+            context.Movies.Add(add);
+            context.SaveChanges();*/
         }
 
         // PUT api/values/5
